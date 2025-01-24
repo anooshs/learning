f491177ce42db51813ca794de3198cfe6604c24c
Anoosh_S <anoosh.sankuru@capillarytech.com>
2025-01-25 01:15:19 +0530

    Learning - Express

---
+++ express/app.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
